539c674c854e4e244d26b953b2805e76961651dd
NoaYnov <113116077+NoaYnov@users.noreply.github.com>
2023-09-20 17:10:37 +0200

    visuel +

--- src/Project Plateformer/Project Plateformer/Project Plateformer/Form1.cs
+++ src/Project Plateformer/Project Plateformer/Project Plateformer/Form1.cs
@@ -16,5 +16,10 @@ namespace Project_Plateformer
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
